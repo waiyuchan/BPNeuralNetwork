431c0626b2da3da8759b624e79407bc071798660
Will <weiyuchens@sina.com>
2022-04-30 06:15:29 +0100

    update

--- BPNN/bpnn.cpp
+++ BPNN/bpnn.cpp
@@ -1,5 +1,36 @@
 #include "bpnn.h"
 
-BPNeuralNetwork::BPNeuralNetwork(){
+BPNeuralNetwork::BPNeuralNetwork()
+{
+    srand((unsigned)time(NULL)); // 随机数种子
+    error = 100.f;
+
+    for (int i = 0; i < input_nodes; ++i)
+    {
+        input_layer[i] = new InputNode();
+        for (int j = 0; j < hidden_nodes; ++j)
+        {
+            input_layer[i]->weight.push_back(get_random_num());
+            input_layer[i]->w_delta_sum.push_back(0.f);
+        }
+    }
+
+    for (int i = 0; i < hidden_layers; i++)
+    {
+        if (i == hidden_layers - 1)
+        {
+            /* code */
+        }
+        
+    }
+    
 
 }
+
+void BPNeuralNetwork::forwardPropagation() {}
+
+void BPNeuralNetwork::backwardPropagation() {}
+
+void BPNeuralNetwork::train() {}
+
+void BPNeuralNetwork::predict() {}
---
+++ BPNN/main.cpp
@@ -0,0 +1,6 @@
+#include "bpnn.h"
+
+int main()
+{
+    return 0;
+}
